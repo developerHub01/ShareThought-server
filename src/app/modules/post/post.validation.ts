da43ee9aa7f300e8ce7961dbc4bce1cbb99ea1e2
developerHub01 <developerhubytchannel@gmail.com>
2024-07-16 03:49:51 +0600

    form-data number and boolean handled for post

--- src/app/modules/post/post.validation.ts
+++ src/app/modules/post/post.validation.ts
@@ -1,3 +1,4 @@
+import { Validation } from "./../../validaton/validation";
 import { z as zod } from "zod";
 import { PostConstant } from "./post.constant";
 
@@ -28,7 +29,12 @@ const createPostValidationSchema = zod.object({
       PostConstant.POST_CONTENT_MIN_LENGTH,
       `post title max length is ${PostConstant.POST_CONTENT_MIN_LENGTH}`,
     ),
-  isPublished: zod.boolean().optional().default(false),
+  isPublished: zod
+    .string()
+    .transform(Validation.parseBoolean)
+    .optional()
+    .default("false")
+    .pipe(zod.boolean()),
   banner: zod.string().array().optional(),
   scheduledTime: zod.string().optional(),
   tags: zod.string().array().optional(),
@@ -63,7 +69,12 @@ const updatePostValidationSchema = zod.object({
       `post title max length is ${PostConstant.POST_CONTENT_MIN_LENGTH}`,
     )
     .optional(),
-  isPublished: zod.boolean().optional().default(false),
+  isPublished: zod
+    .string()
+    .transform(Validation.parseBoolean)
+    .optional()
+    .default("false")
+    .pipe(zod.boolean()),
   banner: zod.string().array().optional(),
   scheduledTime: zod.string().optional(),
   tags: zod.string().array().optional(),
