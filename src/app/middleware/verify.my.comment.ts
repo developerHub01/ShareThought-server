1cb180c8847320bffb9477a2d7ee14f0fe648c17
developerHub01 <developerhubytchannel@gmail.com>
2024-07-26 07:56:50 +0600

    verify.my.comments middleware updated based on activeChannel

--- src/app/middleware/verify.my.comment.ts
+++ src/app/middleware/verify.my.comment.ts
@@ -6,9 +6,13 @@ import { CommentModel } from "../modules/comment/comment.model";
 
 const verifyMyComment = catchAsync(async (req, res, next) => {
   const { id: commentId } = req.params;
-  const { userId } = req as IRequestWithActiveDetails;
+  const { userId, channelId } = req as IRequestWithActiveDetails;
 
-  const isMyComment = await CommentModel.isMyComment(commentId, userId);
+  const isMyComment = await CommentModel.isMyComment(
+    commentId,
+    channelId || userId,
+    channelId ? "channelId" : "userId",
+  );
 
   if (!isMyComment)
     throw new AppError(httpStatus.UNAUTHORIZED, "This is not your comment");
