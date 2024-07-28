1245d9f067ac54a244d38999f1a5c89de13ac2d1
developerHub01 <developerhubytchannel@gmail.com>
2024-07-29 01:44:39 +0600

    verifyMyCommunityPost middleware created

--- src/app/middleware/verify.my.community.post.ts
+++ src/app/middleware/verify.my.community.post.ts
@@ -1,17 +1,17 @@
 import httpStatus from "http-status";
 import AppError from "../errors/AppError";
 import { IRequestWithActiveDetails } from "../interface/interface";
-import { PostModel } from "../modules/post/post.model";
 import catchAsync from "../utils/catch.async";
+import { CommunityModel } from "../modules/community/community.model";
 
 const verifyMyCommunityPost = catchAsync(async (req, res, next) => {
-  const { postId } = req.params;
+  const { communityPostId } = req.params;
   const { channelId } = req as IRequestWithActiveDetails;
 
   if (!channelId)
     throw new AppError(httpStatus.UNAUTHORIZED, "no channel activated");
 
-  const result = await PostModel.isMyPost(postId, channelId);
+  const result = await CommunityModel.isMyPost(communityPostId, channelId);
 
   if (!result)
     throw new AppError(httpStatus.UNAUTHORIZED, "This is not your post");
