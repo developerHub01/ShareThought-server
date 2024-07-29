55e73027cb9277c8e28f1ec320edf5a191a0cc8d
developerHub01 <developerhubytchannel@gmail.com>
2024-07-30 04:08:08 +0600

    req body data reformate based on schema middleware created

--- src/app/modules/community.post/community.post.middleware.ts
+++ src/app/modules/community.post/community.post.middleware.ts
@@ -1,9 +1,9 @@
+import httpStatus from "http-status";
+import AppError from "../../errors/AppError";
 import errorHandler from "../../errors/errorHandler";
 import catchAsync from "../../utils/catch.async";
 import { imageUpload } from "../../utils/multer.image.upload";
-import { validateSchema } from "../../utils/validate.schema";
 import { CommunityPostConstant } from "./community.post.constant";
-import { CommunityPostValidation } from "./community.post.validation";
 
 const createOrUpdatePostImages = imageUpload.fields([
   {
@@ -11,69 +11,95 @@ const createOrUpdatePostImages = imageUpload.fields([
   },
 ]);
 
-const handleCreatePostImagesWithContent = catchAsync(async (req, res, next) => {
-  try {
-    const { postType, images, details } = req.body;
+const matchReqBodyFilesWithValidationSchema = catchAsync(
+  async (req, res, next) => {
+    try {
+      const { postType, images, details } = req.body;
 
-    switch (postType) {
-      case CommunityPostConstant.COMMUNITY_POST_TYPES.IMAGE: {
-        if (!images || !images?.length) break;
+      switch (postType) {
+        case CommunityPostConstant.COMMUNITY_POST_TYPES.IMAGE: {
+          if (!images || !images?.length) break;
 
-        req.body.postImageDetails = {
-          image: images[0],
-        };
+          req.body.postImageDetails = {
+            image: images[0],
+          };
 
-        break;
+          break;
+        }
+        case CommunityPostConstant.COMMUNITY_POST_TYPES.POST_SHARE: {
+          if (!details || !details?.postId) break;
+
+          req.body.postSharedPostDetails = {
+            postId: details.postId,
+          };
+
+          break;
+        }
+        case CommunityPostConstant.COMMUNITY_POST_TYPES.QUIZ: {
+          try {
+            req.body.postQuizDetails = JSON.parse(details);
+          } catch (error) {
+            throw new AppError(
+              httpStatus.BAD_REQUEST,
+              "post details is not valid",
+            );
+          }
+
+          break;
+        }
+        case CommunityPostConstant.COMMUNITY_POST_TYPES.POLL: {
+          try {
+            req.body.postPollDetails = JSON.parse(details);
+          } catch (error) {
+            throw new AppError(
+              httpStatus.BAD_REQUEST,
+              "post details is not valid",
+            );
+          }
+
+          break;
+        }
+        case CommunityPostConstant.COMMUNITY_POST_TYPES.POLL_WITH_IMAGE: {
+          try {
+            const detailsData = JSON.parse(details);
+
+            if (detailsData?.options?.length !== images?.length)
+              throw new AppError(
+                httpStatus.BAD_REQUEST,
+                "post details is not valid",
+              );
+
+            images.map((image: string, index: number) => {
+              if (
+                typeof detailsData?.options[index] === "object" &&
+                detailsData?.options[index] !== null
+              )
+                detailsData.options[index].image = image;
+            });
+
+            req.body.postPollWithImageDetails = detailsData;
+          } catch (error) {
+            throw new AppError(
+              httpStatus.BAD_REQUEST,
+              "post details is not valid",
+            );
+          }
+
+          break;
+        }
       }
-      case CommunityPostConstant.COMMUNITY_POST_TYPES.POST_SHARE: {
-        if (!details || !details?.postId) break;
 
-        req.body.postSharedPostDetails = {
-          postId: details.postId,
-        };
+      delete req.body["details"];
+      delete req.body["images"];
 
-        break;
-      }
-      case CommunityPostConstant.COMMUNITY_POST_TYPES.QUIZ: {
-        const result = await validateSchema(
-          CommunityPostValidation.postQuizDetailsValidationSchema,
-          details,
-        );
-
-        console.log({ result });
-
-        req.body.postQuizDetails = details;
-
-        break;
-      }
-      case CommunityPostConstant.COMMUNITY_POST_TYPES.POLL: {
-        break;
-      }
-      case CommunityPostConstant.COMMUNITY_POST_TYPES.POLL_WITH_IMAGE: {
-        break;
-      }
-      default: {
-        req.body.details = undefined;
-        req.body.images = undefined;
-      }
+      next();
+    } catch (error) {
+      errorHandler(error);
     }
-
-    console.log(req.body);
-    
-
-    const result = await validateSchema(
-      CommunityPostValidation.createPostValidationSchema,
-      req.body,
-    );
-
-    console.log(result);
-    // next();
-  } catch (error) {
-    errorHandler(error);
-  }
-});
+  },
+);
 
 export const CommunityPostMiddleware = {
   createOrUpdatePostImages,
-  handleCreatePostImagesWithContent,
+  matchReqBodyFilesWithValidationSchema,
 };
