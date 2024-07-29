a0f9c53eeb19a7c5b3839d180f88e6d6d067da1a
developerHub01 <developerhubytchannel@gmail.com>
2024-07-29 19:59:41 +0600

    minor ts bug fixed

--- src/app/middleware/global.error.handler.ts
+++ src/app/middleware/global.error.handler.ts
@@ -4,6 +4,7 @@ export const globalErrorHandler = (
   error: any,
   req: Request,
   res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   next: NextFunction
 ) => {
   const statusCode = error.statusCode || 500;
