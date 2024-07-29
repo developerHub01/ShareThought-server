738ab78ad7b615af2767b69400ed85fb92b06e4f
developerHub01 <developerhubytchannel@gmail.com>
2024-07-29 06:50:40 +0600

    community route defined

--- src/app/modules/community/community.route.ts
+++ src/app/modules/community/community.route.ts
@@ -1,5 +1,45 @@
 import express from "express";
+import { CommunityController } from "./community.controller";
+import getLoggedInUser from "../../middleware/get.loggedin.user";
+import getActiveChannel from "../../middleware/get.active.channel";
+import verifyMyCommunityPost from "../../middleware/verify.my.community.post";
 
 const router = express.Router();
+
+router.get("/", CommunityController.findCommuityPosts);
+
+router.get("/channel/:id", CommunityController.findCommuityPostsByChannelId);
+
+router.get(
+  "/my",
+  getLoggedInUser,
+  getActiveChannel,
+  CommunityController.findCommuityPostsByChannelId,
+);
+
+router.get("/:id", CommunityController.findCommuityPostById);
+
+router.post(
+  "/",
+  getLoggedInUser,
+  getActiveChannel,
+  CommunityController.createPost,
+);
+
+router.patch(
+  "/:id",
+  getLoggedInUser,
+  getActiveChannel,
+  verifyMyCommunityPost,
+  CommunityController.updatePost,
+);
+
+router.delete(
+  "/:id",
+  getLoggedInUser,
+  getActiveChannel,
+  verifyMyCommunityPost,
+  CommunityController.deletePost,
+);
 
 export const CommunityRoutes = router;
