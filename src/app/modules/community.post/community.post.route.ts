a446b8a3dc038d1ca800c34cf74857cb1b868067
developerHub01 <developerhubytchannel@gmail.com>
2024-07-30 07:11:09 +0600

    post find, create, update, delete route added

--- src/app/modules/community.post/community.post.route.ts
+++ src/app/modules/community.post/community.post.route.ts
@@ -3,12 +3,19 @@ import { CommunityPostController } from "./community.post.controller";
 import getLoggedInUser from "../../middleware/get.loggedin.user";
 import getActiveChannel from "../../middleware/get.active.channel";
 import verifyMyCommunityPost from "../../middleware/verify.my.community.post";
+import { CommunityPostMiddleware } from "./community.post.middleware";
+import { CommunityPostValidation } from "./community.post.validation";
+import { validateRequest } from "../../middleware/validate.request";
+import readReqBodyFiles from "../../middleware/read.req.body.files";
 
 const router = express.Router();
 
 router.get("/", CommunityPostController.findCommuityPosts);
 
-router.get("/channel/:id", CommunityPostController.findCommuityPostsByChannelId);
+router.get(
+  "/channel/:id",
+  CommunityPostController.findCommuityPostsByChannelId,
+);
 
 router.get(
   "/my",
@@ -21,6 +28,10 @@ router.get("/:id", CommunityPostController.findCommuityPostById);
 
 router.post(
   "/",
+  CommunityPostMiddleware.createOrUpdatePostImages,
+  readReqBodyFiles /* read files info from formData */,
+  CommunityPostMiddleware.matchReqBodyFilesWithValidationSchema,
+  validateRequest(CommunityPostValidation.createPostValidationSchema),
   getLoggedInUser,
   getActiveChannel,
   CommunityPostController.createPost,
