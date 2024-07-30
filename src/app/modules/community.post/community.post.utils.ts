80101bf8d175c73e39f3e6caaea68af0423f0d8b
developerHub01 <developerhubytchannel@gmail.com>
2024-07-30 06:56:24 +0600

    createOrUpdatePostImage added for uploading image in cloudinary

--- src/app/modules/community.post/community.post.utils.ts
+++ src/app/modules/community.post/community.post.utils.ts
@@ -1,1 +1,27 @@
-export const CommunityPostUtils = {};
+import errorHandler from "../../errors/errorHandler";
+import { CloudinaryUtils } from "../../utils/cloudinary.utils";
+
+const createOrUpdatePostImage = async (
+  imagePath: string,
+  cloudinaryMediaPath: string,
+  isUpdating: boolean = false,
+  previousImage?: string,
+) => {
+  try {
+    if (isUpdating && previousImage)
+      await CloudinaryUtils.deleteFile([previousImage]);
+
+    const coverImageDetails = await CloudinaryUtils.uploadFile(
+      imagePath,
+      cloudinaryMediaPath,
+    );
+
+    return coverImageDetails?.secure_url;
+  } catch (error) {
+    return errorHandler(error);
+  }
+};
+
+export const CommunityPostUtils = {
+  createOrUpdatePostImage,
+};
