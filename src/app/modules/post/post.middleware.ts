8b210aca31542a580ad96b608e7ac8dd1eb61921
developerHub01 <developerhubytchannel@gmail.com>
2024-08-26 12:22:44 +0600

    isValidTags middleware added

--- src/app/modules/post/post.middleware.ts
+++ src/app/modules/post/post.middleware.ts
@@ -1,5 +1,7 @@
+import httpStatus from "http-status";
 import catchAsync from "../../utils/catch.async";
 import { imageUpload } from "../../utils/multer.image.upload";
+import AppError from "../../errors/AppError";
 
 const createOrUpdatePostImages = imageUpload.fields([
   {
@@ -15,12 +17,27 @@ const matchReqBodyFilesWithValidationSchema = catchAsync(
     if (req?.body?.banner?.length) bannerPath = req?.body?.banner[0];
 
     if (bannerPath) req.body.banner = bannerPath;
-    
+
     next();
   },
 );
 
+const isValidTags = catchAsync(async (req, res, next) => {
+  const { tags } = req.body;
+
+  tags.forEach((tag: string, index: number) => {
+    tag = tag.trim();
+    if (tags.split(" ").length)
+      throw new AppError(httpStatus.BAD_REQUEST, `${tag} is not a valid`);
+
+    tags[index] = tag;
+  });
+
+  next();
+});
+
 export const PostMiddleware = {
   createOrUpdatePostImages,
   matchReqBodyFilesWithValidationSchema,
+  isValidTags,
 };
