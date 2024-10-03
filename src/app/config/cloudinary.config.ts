aeadcaccb28582f838a7560bf4e36d8f30203347
developerHub01 <developerhubytchannel@gmail.com>
2024-10-04 00:53:09 +0600

    cloudinary small bug fixed

--- src/app/config/cloudinary.config.ts
+++ src/app/config/cloudinary.config.ts
@@ -1,5 +1,5 @@
-import cloudinary from "cloudinary";
-import config from ".";
+import { v2 as cloudinary } from "cloudinary";
+import config from "../config";
 
 interface ICloudinaryConfigOptions {
   cloud_name: string;
