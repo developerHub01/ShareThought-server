4c84f2a98ee4c098a8cd1f43e36ad62e932c2d3e
developerHub01 <developerhubytchannel@gmail.com>
2024-10-10 21:48:56 +0600

    isValidChannel middleware created

---
+++ src/app/middleware/is.valid.channel.ts
@@ -0,0 +1,17 @@
+import AppError from "../errors/AppError";
+import httpStatus from "http-status";
+import catchAsync from "../utils/catch.async";
+import { ChannelModel } from "../modules/channel/model/model";
+
+const isValidChannel = catchAsync(async (req, res, next) => {
+  const { channelId } = req.params;
+
+  const isChannelExist = await ChannelModel.isChannelExist(channelId);
+
+  if (!isChannelExist)
+    throw new AppError(httpStatus.NOT_FOUND, "channel not found");
+
+  return next();
+});
+
+export default isValidChannel;
