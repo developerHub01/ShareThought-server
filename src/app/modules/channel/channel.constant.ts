3cfc0b36b1c7637b53fcc1a6b3108f74610dd9eb
developerHub01 <developerhubytchannel@gmail.com>
2024-10-15 22:02:12 +0600

    update: code updated for CHANNEL_DEFAULT_COVER_LIST

--- src/app/modules/channel/channel.constant.ts
+++ src/app/modules/channel/channel.constant.ts
@@ -1,5 +1,3 @@
-import { getStaticFileURL } from "../../utils/get.static.file.url";
-
 const CHANNEL_COLLECTION_NAME = "Channel";
 
 const CHANNEL_SEARCHABLE_FIELD: Array<string> = [
@@ -12,10 +10,17 @@ const POST_REDIS_TTL = 24 * 60 * 60;
 const CHANNEL_NAME_MAX_LENGTH = 50;
 const CHANNEL_NAME_MIN_LENGTH = 1;
 const CHANNEL_DESCRIPTION_MAX_LENGTH = 250;
-const CHANNEL_DEFAULT_COVER = getStaticFileURL(
-  "cover_placeholder",
-  Math.ceil(Math.random() * 5) + ".jpg",
-);
+
+const CHANNEL_DEFAULT_COVER_LIST = [
+  "https://res.cloudinary.com/ddof4ku9y/image/upload/v1722025201/shareThought/channel/cover_placeholder/1.jpg",
+  "https://res.cloudinary.com/ddof4ku9y/image/upload/v1729001661/shareThought/channel/cover_placeholder/2.jpg",
+  "https://res.cloudinary.com/ddof4ku9y/image/upload/v1722025199/shareThought/channel/cover_placeholder/3.jpg",
+  "https://res.cloudinary.com/ddof4ku9y/image/upload/v1721171199/shareThought/channel/cover_placeholder/4.jpg",
+  "https://res.cloudinary.com/ddof4ku9y/image/upload/v1721171199/shareThought/channel/cover_placeholder/5.jpg",
+];
+
+const CHANNEL_DEFAULT_COVER = () =>
+  ChannelConstant.CHANNEL_DEFAULT_COVER_LIST[Math.ceil(Math.random() * 5)];
 
 const CHANNEL_AVATAR_SIZE = {
   WIDTH: 400,
@@ -33,6 +38,7 @@ export const ChannelConstant = {
   CHANNEL_NAME_MAX_LENGTH,
   CHANNEL_NAME_MIN_LENGTH,
   CHANNEL_DESCRIPTION_MAX_LENGTH,
+  CHANNEL_DEFAULT_COVER_LIST,
   CHANNEL_DEFAULT_COVER,
   CHANNEL_SEARCHABLE_FIELD,
   CHANNEL_AVATAR_SIZE,
