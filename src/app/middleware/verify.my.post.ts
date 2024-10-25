6025b32b210d43d126f44953ddfbf266e54a2787
developerHub01 <developerhubytchannel@gmail.com>
2024-10-25 23:59:51 +0600

    update: veryMyPost for both blogPost and CommunityPost

--- src/app/middleware/verify.my.post.ts
+++ src/app/middleware/verify.my.post.ts
@@ -3,17 +3,24 @@ import AppError from "../errors/AppError";
 import { IRequestWithActiveDetails } from "../interface/interface";
 import catchAsync from "../utils/catch.async";
 import { PostModel } from "../modules/post/model/model";
+import { CommunityPostModel } from "../modules/community.post/model/model";
 
 const verifyMyPost = catchAsync(async (req, res, next) => {
-  const { postId } = req.params;
+  const { postId, communityPostId } = req.params;
   const { channelId } = req as IRequestWithActiveDetails;
 
   if (!channelId)
     throw new AppError(httpStatus.UNAUTHORIZED, "no channel activated");
 
-  const result = await PostModel.isMyPost(postId, channelId);
+  let postData;
 
-  if (!result)
+  if (postId) postData = await PostModel.findById(postId);
+  else if (communityPostId)
+    postData = await CommunityPostModel.findById(postId);
+
+  if (!postData) throw new AppError(httpStatus.NOT_FOUND, "post not found");
+
+  if (postData.channelId?.toString() !== channelId)
     throw new AppError(httpStatus.UNAUTHORIZED, "This is not your post");
 
   return next();
